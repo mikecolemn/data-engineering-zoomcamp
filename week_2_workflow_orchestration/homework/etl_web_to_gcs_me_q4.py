f7df89385f7b7a5c3c3d32e80fde62b817f1c8c1
Mike Cole <mike.cole.mn@gmail.com>
2023-02-05 08:48:29 -0600

    path updates for Q4

--- week_2_workflow_orchestration/homework/etl_web_to_gcs_me_q4.py
+++ week_2_workflow_orchestration/homework/etl_web_to_gcs_me_q4.py
@@ -26,7 +26,7 @@ def clean(df = pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file:str) -> Path:
     """Write DataFrame out locally as a parquet file"""
-    path = Path(f"../data/{color}/{dataset_file}.parquet")
+    path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path  
 
