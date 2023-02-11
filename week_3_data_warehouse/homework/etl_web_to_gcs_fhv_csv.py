8276b2bd5be0921bc2cf2bd43e6a8ebc7152ad65
Mike Cole <mike.cole.mn@gmail.com>
2023-02-10 20:36:03 -0600

    updates

--- week_3_data_warehouse/homework/etl_web_to_gcs_fhv_csv.py
+++ week_3_data_warehouse/homework/etl_web_to_gcs_fhv_csv.py
@@ -7,8 +7,9 @@ from prefect_gcp.cloud_storage import GcsBucket
 @task(retries=3)
 def retrieve_file(dataset_url: str, color:str, dataset_file: str) -> Path:
     """Retrieve data files from github repo"""
+    
+    # print(os.system("pwd"))
     os.system(f"wget {dataset_url} -O data/{color}/{dataset_file}")
-    print(os.system("pwd"))
     file_path = Path(f"data/{color}/{dataset_file}")
 
     return file_path
