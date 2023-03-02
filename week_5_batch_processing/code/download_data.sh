95a87a0c21b0f5024de06af77d3972350c4724cd
Mike Cole <mike.cole.mn@gmail.com>
2023-03-01 20:02:55 -0600

    week 5 homework content

--- week_5_batch_processing/code/download_data.sh
+++ week_5_batch_processing/code/download_data.sh
@@ -3,12 +3,15 @@ set -e
 
 # https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2020-01.csv.gz
 
-TAXI_TYPE=$1  #"yellow"
-YEAR=$2 # 2020
+
+TAXI_TYPE=$1  #"fhvhv"
+YEAR=$2 # 2021
 
 URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
 
-for MONTH in {1..12}; do
+
+# for MONTH in {1..12}; do
+for MONTH in {6}; do
     FMONTH=`printf "%02d" ${MONTH}`
 
     URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
